501a82c8cba2e31151549fdc4ae9c91c2019e71c
Rodrigo-Tovar <rodrigo.tovar253226@potros.itson.edu.mx>
2026-09-12 21:14:22 -0700

    formateado del código

--- src/main/java/mx/edu/campuscare/common/ApiErrorHandler.java
+++ src/main/java/mx/edu/campuscare/common/ApiErrorHandler.java
@@ -1,3 +1,13 @@
 package mx.edu.campuscare.common;
-import org.springframework.http.*; import org.springframework.web.bind.annotation.*; import java.util.Map;
-@RestControllerAdvice public class ApiErrorHandler { @ExceptionHandler(Exception.class) ResponseEntity<Map<String,String>> error(Exception e){return ResponseEntity.status(500).body(Map.of("error",e.toString()));} } // TRAINING GAP U2-P4: expone detalle.
+
+import org.springframework.http.*;
+import org.springframework.web.bind.annotation.*;
+import java.util.Map;
+
+@RestControllerAdvice
+public class ApiErrorHandler {
+    @ExceptionHandler(Exception.class)
+    ResponseEntity<Map<String, String>> error(Exception e) {
+        return ResponseEntity.status(500).body(Map.of("error", e.toString()));
+    }
+} // TRAINING GAP U2-P4: expone detalle.
--- src/main/java/mx/edu/campuscare/tickets/TicketRepository.java
+++ src/main/java/mx/edu/campuscare/tickets/TicketRepository.java
@@ -1,3 +1,6 @@
 package mx.edu.campuscare.tickets;
+
 import org.springframework.data.jpa.repository.JpaRepository;
-public interface TicketRepository extends JpaRepository<Ticket,Long>{}
+
+public interface TicketRepository extends JpaRepository<Ticket, Long> {
+}
